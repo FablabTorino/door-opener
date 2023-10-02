e730d449ea16fefd165fa047e7e4c727ccea410e
Matteo Giaccone <matteo@turnintocoders.it>
2023-10-02 21:24:35 +0200

    bot updates

--- TelegramBot/telegrambot.sh
+++ TelegramBot/telegrambot.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+killall mosquitto_sub
+mosquitto_sub -h 127.0.0.1 -t esp-rfid/cmd >> /home/pi/door-opener/TelegramBot/mosquitto_esp-rfid_cmd_sub &
+mosquitto_sub -h 127.0.0.1 -t esp-rfid/send >> /home/pi/door-opener/TelegramBot/mosquitto_esp-rfid_send_sub &
 
 pushd ${BASH_SOURCE%/*}
 
